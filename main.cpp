ae2730905f308cca3412764ebd868d74e3e9fa30
baronialexandre <kebabman@hotmail.fr>
2017-01-29 10:40:57 +0100

    Added text file redirection to convert the maze from txt to png
    using the java thing i did (coming soon)

--- main.cpp
+++ main.cpp
@@ -3,21 +3,31 @@
 #include "clabyrinthe.h"
 #include "ccase.h"
 #include <utility>
-
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    ofstream fw;
+    fw.open("text.txt");
+    std::streambuf *oldbuf = std::cout.rdbuf(); //save
+    std::cout.rdbuf(fw.rdbuf());
+
     //CLabyrinthe john(12, 12);//COOOOL
     CLabyrinthe john(79, 42); //my terminal max
+    //CLabyrinthe john(100,100);
     //CLabyrinthe john(3000,3000); // need more then 2000000 taille de la reserve de la pile (equivalent de 2Go de ram)
     john.GenLaby();
     //john.Solve();
     //john.Afficher();
-    cout<<endl;
+
     john.AfficherPlusChemin();
+
+    //reset back to standard input
+    std::cout.rdbuf(oldbuf);
+    cout<<endl;
     int non;
-    cin>>non;
+    //cin>>non;
     return 0;
 }
